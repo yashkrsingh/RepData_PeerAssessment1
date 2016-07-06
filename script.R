46fbd7543c38cb86422d4e9332df683de2cb4a2b
Yash Kumar Singh <yashkr.singh@gmail.com>
2016-07-06 20:40:11 +0530

    Add files via upload

---
+++ script.R
@@ -0,0 +1,42 @@
+## Name         : Yash Kumar Singh
+## Program Title: Reproducible Research (Temporary)
+## ------------------------------------------------------------------
+
+library(ggplot2)
+data <- read.csv("~/Reproducible-Research-Assignment-1-Coursera/activity.csv")
+omit <- na.omit(data)
+test <- aggregate(steps ~ date, omit, sum)
+ggplot(data = test, aes(test$steps)) + geom_histogram(bins = 20,col = "black", fill = "blue", alpha = 0.7) + labs(x = "Number of steps", y = "Frequency", title = "Number of Steps Taken Each Day")
+m1 <- aggregate(steps ~ date, omit, mean)
+m2 <- aggregate(steps ~ date, omit, median)
+
+test <- aggregate(steps ~ interval, omit, mean)
+g <- ggplot(data = test, aes(test$interval, test$steps)) 
+g <- g + geom_line(col = "red")
+g <- g + labs(x = "Time Intervals", y = "Average Steps", title = "Average Number Of Steps Taken Across All Days")
+print(g)
+
+length(which(is.na(data$steps)))
+newdata <- data
+newdata <- merge(aggregate(steps ~ interval, data, mean), newdata, by = "interval", all.x = TRUE, all.y = TRUE)
+newdata <- newdata[order(newdata$date), ]
+for(i in 1:nrow(newdata)){
+  if(is.na(newdata$steps.y[i])){
+    newdata$steps.y[i] <- newdata$steps.x[i]
+  }
+}
+newdata <- newdata[, c(1,3,4)]
+colnames(newdata) <- c('interval','steps','date')
+
+omit[,2] <- as.Date(omit[ ,2])
+index <- which(weekdays(omit[,2])=="Sunday" | weekdays(omit[,2])=="Saturday")
+omit$weekday <- "weekday"
+omit[index, 4] <- "weekend"
+omit$weekday <- as.factor(omit$weekday)
+rm(index)
+test <- aggregate(steps ~ interval + weekday, omit, mean)
+g <- ggplot(data = test, aes(interval, steps))
+g <- g + geom_line(col = "red") 
+g <- g + facet_grid(. ~ weekday)
+g <- g + labs(x = "Time Intervals", y = "Average Steps", title = "Difference Between Weekdays and Weekends")
+print(g)
